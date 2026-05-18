82b979a6b50826ad108367fdad60249676bee1a7
185nico007 <nicolas.delamora@cirvianum.cat>
2026-05-18 12:56:30 +0200

    Update login.php

--- LDAP/web/login.php
+++ LDAP/web/login.php
@@ -1,8 +1,4 @@
 <?php
-/**
- * Script d'autenticació contra servidor LDAP
- * Projecte: nicodaw.cat
- */
 
 $ldap_server = "localhost";
 $base_dn = "dc=nicodaw,dc=local";
@@ -11,18 +7,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $user = $_POST['username'];
     $pass = $_POST['password'];
 
-    // 1. Connexió al servidor
+    // Connexió al servidor
     $ldap_conn = ldap_connect($ldap_server);
     ldap_set_option($ldap_conn, LDAP_OPT_PROTOCOL_VERSION, 3);
 
-    // 2. Construïm el DN de l'usuari segons la teva nova estructura
-    // Tots pengen de ou=users, independentment del seu grup posix
+    // Construïm el DN de l'usuari segons la estructura
     $user_dn = "uid=$user,ou=users,$base_dn";
 
-    // 3. Intentem l'autenticació
+    //  Intentem l'autenticació
     if (@ldap_bind($ldap_conn, $user_dn, $pass)) {
         header("Location: success.html");
     } else {
+        //Si l'usuari es incorrecte surt aixo
         echo "<script>alert('Error: Usuari o contrasenya incorrectes'); window.location='index.html';</script>";
     }
     
